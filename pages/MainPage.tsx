291210d84837ff0942194170e04e413388d6adb9
DAEL64 <qoreqore001@gmail.com>
2026-02-01 21:15:39 +0400

    redesigned main page.

--- pages/MainPage.tsx
+++ pages/MainPage.tsx
@@ -54,10 +54,14 @@ const MainPage = () => {
                   </Link>
                 </div>
               </div>
+              <div className="relative opacity-30 italic w-full text-[200px] text-gray-300 font-bold">
+                <span className="absolute lg:block hidden -top-50 right-1/10">
+                  {categoryLabels[key]}
+                </span>
+              </div>
 
               <div className="w-full px-5 sm:px-10 md:px-20 lg:px-32 mx-auto relative">
                 <div className="flex flex-col lg:block relative">
-      
                   <div className="lg:w-[70%] mb-10 lg:mb-0">
                     {items.slice(1, 2).map((item, index) => (
                       <div
@@ -102,6 +106,15 @@ const MainPage = () => {
                   </div>
 
                   <div className="lg:w-[70%] lg:absolute lg:right-0 pt-10 pb-10 lg:pb-0 z-10">
+                    <div className="hidden lg:flex lg:absolute lg:-left-60 xl:-left-80 bottom-1/4 lg:col-span-2 lg:col-start-6 lg:row-start-1 items-start pt-60 justify-center z-10">
+                      <div className="p-4 text-center border-y border-[#C5A02E]/30">
+                        <span className="block font-sans text-[11px] tracking-[0.3em] uppercase text-[#1A1A1A]/60 mb-2">
+                          Curated
+                        </span>
+                        <div className="w-px h-12 bg-[#C5A02E] mx-auto mb-2" />
+                        <span className="italic text-[#C5A02E] text-xl">&</span>
+                      </div>
+                    </div>
                     {items.slice(2, 3).map((item) => (
                       <div
                         className={`${Styles.revealRight} ${Styles.menuCard} flex flex-col md:flex-row gap-6 p-6 rounded-lg`}
@@ -143,7 +156,7 @@ const MainPage = () => {
                     ))}
                   </div>
 
-                  <div className="lg:w-[70%] lg:pt-115">
+                  <div className="lg:w-[70%] lg:pt-95 2xl:pt-110 ">
                     {items.slice(0, 1).map((item, index) => (
                       <div
                         className={`${Styles.revealLeft} ${Styles.menuCard} flex flex-col md:flex-row gap-6 p-6 rounded-lg`}
@@ -179,11 +192,26 @@ const MainPage = () => {
                           </p>
 
                           <span
-                            className={`${Styles.dividerLine} border-b-2 border-b-[(#E32929)]`}
+                            className={`${Styles.dividerLine} border-b-2 border-b-[#E32929]`}
                           />
                         </div>
                       </div>
                     ))}
+                    <div className="hidden lg:flex lg:col-span-4 lg:col-start-8 lg:row-start-1 pt-40 px-8">
+                      <div className="space-y-6">
+                        <p className="text-[#1A1A1A]/60 italic text-xl leading-relaxed border-l-2 border-[#C5A02E] pl-6">
+                          Our {key.toLowerCase()} are curated to evoke a sense
+                          of home, reimagined through modern technique and local
+                          sourcing.
+                        </p>
+                        <div className="flex items-center gap-4 text-[#C5A02E]">
+                          <span className="text-[10px] uppercase tracking-widest font-sans font-bold">
+                            Signature
+                          </span>
+                          <div className="w-12 h-px bg-[#C5A02E]" />
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
